233018412ee3248ee4a632f8b56208af3e2e0a57
lopho <lopho@users.noreply.github.com>
2017-01-21 19:19:27 +0100

    removed hardcoded absolute paths from cmake config

--- kinect2Config.cmake
+++ kinect2Config.cmake
@@ -4,13 +4,13 @@ find_package(freenect2 REQUIRED)
 include_directories(SYSTEM ${freenect2_INCLUDE_DIRS})
 
 FIND_LIBRARY(kinect2_LIBRARY kinect2
-    PATHS /home/zweistecken/Dokumente/Development/projection_mapping/workspace/libkinect2/install/lib
+    PATHS "${kinect2_DIR}/lib"
     NO_DEFAULT_PATH
 )
 SET(kinect2_LIBRARIES "${kinect2_LIBRARY};${freenect2_LIBRARIES}")
 
 FIND_PATH(kinect2_INCLUDE_DIR kinect2/Kinect2.h
-    PATHS /home/zweistecken/Dokumente/Development/projection_mapping/workspace/libkinect2/install/include
+    PATHS "${kinect2_DIR}/include"
     NO_DEFAULT_PATH
 )
 SET(kinect2_INCLUDE_DIRS "${kinect2_INCLUDE_DIR};${freenect2_INCLUDE_DIRS}")
